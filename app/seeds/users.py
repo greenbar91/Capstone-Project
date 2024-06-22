42d2a4d11b98053b4b89c52842acad270a3e35a7
Jacob Dietz <jdietz981@gmail.com>
2024-06-21 22:44:29 -0500

    Added Book seed data

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -7,13 +7,55 @@ def seed_users():
     demo = User(
         username='Demo', email='demo@aa.io', password='password', profile_pic="placeholder.png")
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password', profile_pic="placeholder.png")
+        username='Marnie Gardens', email='marnie@aa.io', password='password', profile_pic="placeholder.png")
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password', profile_pic="placeholder.png")
+        username='Bobbie Smith', email='bobbie@aa.io', password='password', profile_pic="placeholder.png")
+    jacob = User(
+        username="Jacob Gonzalez", email="jacob@aa.io", password="password", profile_pic="placeholder.png"
+    )
+    sarah = User(
+        username="Sarah Stile", email="sarah@aa.io", password="password", profile_pic="placeholder.png"
+    )
+    brandon = User(
+        username="Brandon Sanderson", email="brandon@aa.io", password="password", profile_pic="placeholder.png"
+    )
+    zogarth = User(
+        username="Zogarth", email="zogarth@aa.io", password="password", profile_pic="placeholder.png"
+    )
+    willwight = User(
+        username="Will Wight", email="willw@aa.io", password="password", profile_pic="placeholder.png"
+
+    )
+    brentweeks = User(
+        username="Brent Weeks", email="brentw@aa.io", password="password", profile_pic="placeholder.png"
+    )
+    tamsyn = User(
+        username="Tamsyn Muir", email="tamsyn@aa.io", password="password", profile_pic="placeholder.png"
+    )
+    robinhobb = User(
+        username="Robin Hobb", email="robin@aa.io", password="password", profile_pic="placeholder.png"
+    )
+    jimbutcher = User(
+        username="Jim Butcher", email="jimb@aa.io", password="password", profile_pic="placeholder.png"
+    )
+    margaretweis = User(
+        username="Margaret Weis", email="margw@aa.io", password="password", profile_pic="placeholder.png"
+
+    )
 
     db.session.add(demo)
     db.session.add(marnie)
     db.session.add(bobbie)
+    db.session.add(jacob)
+    db.session.add(sarah)
+    db.session.add(brandon)
+    db.session.add(zogarth)
+    db.session.add(willwight)
+    db.session.add(brentweeks)
+    db.session.add(tamsyn)
+    db.session.add(robinhobb)
+    db.session.add(jimbutcher)
+    db.session.add(margaretweis)
     db.session.commit()
 
 
