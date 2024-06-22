32bd7e4d8e8f0fb5e658ebe09967608f23d05a9a
Jacob Dietz <jdietz981@gmail.com>
2024-06-22 17:54:27 -0500

    Added chapter seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .books import seed_books, undo_books
+from .chapters import seed_chapters, undo_chapters
 
 from app.models.db import db, environment, SCHEMA
 
@@ -17,16 +18,19 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_chapters()
         undo_books()
         undo_users()
     seed_users()
     seed_books()
+    seed_chapters()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_chapters()
     undo_books()
     undo_users()
     # Add other undo functions here
