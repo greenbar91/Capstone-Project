c1884ba6825d01245ba0e26503db1894729b4319
Jacob Dietz <jdietz981@gmail.com>
2024-06-22 18:55:08 -0500

    Added favorite seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -5,6 +5,7 @@ from .chapters import seed_chapters, undo_chapters
 from .comments import seed_comments, undo_comments
 from .reviews import seed_reviews, undo_reviews
 from .tags import seed_tags, undo_tags
+from .favorites import seed_favorites, undo_favorites
 
 from app.models.db import db, environment, SCHEMA
 
@@ -21,6 +22,7 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_favorites()
         undo_tags()
         undo_reviews()
         undo_comments()
@@ -33,12 +35,14 @@ def seed():
     seed_comments()
     seed_reviews()
     seed_tags()
+    seed_favorites()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_favorites()
     undo_tags()
     undo_reviews()
     undo_comments()
