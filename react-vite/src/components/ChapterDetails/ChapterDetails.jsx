951e824bcefae49e98e1203cb47fb05d43432713
Jacob Dietz <jdietz981@gmail.com>
2024-07-08 17:24:11 -0500

    Added chapter details styling

--- react-vite/src/components/ChapterDetails/ChapterDetails.jsx
+++ react-vite/src/components/ChapterDetails/ChapterDetails.jsx
@@ -2,11 +2,15 @@ import "./ChapterDetails.css";
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import Comments from "../Comments";
+import { useSelector } from "react-redux";
+import { selectAllReviews } from "../../redux/review";
+import StarRating from "../StarRating/StarRating";
 
 function ChapterDetails() {
   const [chapterObject, setChapterObject] = useState({});
   const [chapters, setChapters] = useState([]);
   const { bookId, chapterId } = useParams();
+  const reviews = useSelector(selectAllReviews);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -36,9 +40,15 @@ function ChapterDetails() {
     return text.replace(/\n/g, "<br>");
   };
 
-  const currentChapterIndex = chapters.findIndex((chapter) => chapter.id === parseInt(chapterId));
-  const previousChapterId = currentChapterIndex > 0 ? chapters[currentChapterIndex - 1]?.id : null;
-  const nextChapterId = currentChapterIndex < chapters.length - 1 ? chapters[currentChapterIndex + 1]?.id : null;
+  const currentChapterIndex = chapters.findIndex(
+    (chapter) => chapter.id === parseInt(chapterId)
+  );
+  const previousChapterId =
+    currentChapterIndex > 0 ? chapters[currentChapterIndex - 1]?.id : null;
+  const nextChapterId =
+    currentChapterIndex < chapters.length - 1
+      ? chapters[currentChapterIndex + 1]?.id
+      : null;
 
   const handlePreviousClick = () => {
     if (previousChapterId) {
@@ -56,21 +66,58 @@ function ChapterDetails() {
     navigate(`/books/${bookId}`);
   };
 
+  const getAverageRating = () => {
+    if (!reviews || reviews.length === 0) return 0;
+    const totalRating = reviews.reduce(
+      (acc, review) => acc + review.star_rating,
+      0
+    );
+    return totalRating / reviews.length;
+  };
+
+  const averageRating = getAverageRating();
+
   return (
-    <div>
-      <h2>{chapterObject.title}</h2>
+    <div className="chapter-details-container">
+      <div className="chapter-header-container">
+        <div className="book-cover-art-container">
+          <img src={chapterObject.cover_art} />
+          <StarRating rating={averageRating} />
+        </div>
+        <header className="book-and-chapter-header-info">
+          <h3 style={{ padding: "5px" }}>{chapterObject.book_title}</h3>
+          <h4 style={{ padding: "5px" }}>by {chapterObject.author_name}</h4>
+          <h2 style={{ padding: "5px" }}> {chapterObject.title}</h2>
+        </header>
+        <div className="donate-and-back-to-book-details">
+          <button className="index-button" onClick={handleBackToIndexClick}>
+            Back to Index
+          </button>
+          <button className="donate-button">Donate</button>
+        </div>
+      </div>
 
       {chapterObject.body && (
-        <div dangerouslySetInnerHTML={{ __html: convertNewlinesToBr(chapterObject?.body) }} />
+        <div className="chapter-body-container">
+          <div
+            dangerouslySetInnerHTML={{
+              __html: convertNewlinesToBr(chapterObject?.body),
+            }}
+          />
+        </div>
       )}
 
-      <div className="chapter-navigation">
-        <button onClick={handlePreviousClick} disabled={!previousChapterId}>Previous</button>
-        <button onClick={handleNextClick} disabled={!nextChapterId}>Next</button>
-        <button onClick={handleBackToIndexClick}>Back to Index</button>
+      <div className="chapter-navigation-container">
+        <button className="prev-button" onClick={handlePreviousClick} disabled={!previousChapterId}>
+          Previous
+        </button>
+        <button className="back-to-index-button" onClick={handleBackToIndexClick}>Back to Index</button>
+        <button className="next-button" onClick={handleNextClick} disabled={!nextChapterId}>
+          Next
+        </button>
       </div>
       <div>
-        <Comments chapterId={chapterObject.id}/>
+        <Comments chapterId={chapterObject.id} />
       </div>
     </div>
   );
--- react-vite/src/components/Comments/Comments.jsx
+++ react-vite/src/components/Comments/Comments.jsx
@@ -9,6 +9,7 @@ import {
 import { useEffect, useState } from "react";
 import OpenModalButton from "../OpenModalButton/OpenModalButton";
 import CommentModal from "../CommentModal";
+import { formatDistanceToNow } from "date-fns";
 
 function Comments({ chapterId }) {
   const dispatch = useDispatch();
@@ -21,53 +22,80 @@ function Comments({ chapterId }) {
   }, [chapterId, dispatch]);
 
   const handleDeleteCommentClick = (commentId) => {
-      dispatch(deleteCommentThunk(chapterId, commentId))
-  }
+    dispatch(deleteCommentThunk(chapterId, commentId));
+  };
 
   const handleOnSubmitComment = (e) => {
-    e.preventDefault()
-    const newComment = {user_id:user.id, chapter_id:chapterId, body:body, created_at:new Date()}
-    dispatch(postCommentThunk(chapterId, newComment))
-    setBody('')
+    e.preventDefault();
+    const newComment = {
+      user_id: user.id,
+      chapter_id: chapterId,
+      body: body,
+      created_at: new Date(),
+    };
+    dispatch(postCommentThunk(chapterId, newComment));
+    setBody("");
+  };
+
+  const getTimeAgo = (createdAt) => {
+    return formatDistanceToNow(new Date(createdAt), { addSuffix: true });
   };
 
   return (
-    <div>
-      <ul>
-        <div>
-          <form onSubmit={handleOnSubmitComment}>
-            <div>
-              <label>Leave a comment</label>
-            </div>
-            <div>
-              <textarea
-                value={body}
-                type="text"
-                onChange={(e) => setBody(e.target.value)}
-              />
-            </div>
-            <div>
-              <button type="submit">Post</button>
-            </div>
-          </form>
-        </div>
+    <div className="comments-container">
+      <div className="post-comment-container">
+        <form onSubmit={handleOnSubmitComment}>
+          <div>
+            <label style={{ color: "gray" }}>Comments({comments.length})</label>
+          </div>
+          <div className="comment-text-area-container">
+            <textarea
+              value={body}
+              type="text"
+              onChange={(e) => setBody(e.target.value)}
+              className="comment-text-area"
+            />
+          </div>
+          <div className="post-comment-button-container">
+            <button
+              disabled={body.length === 0}
+              className="post-comment-button"
+              type="submit"
+            >
+              Post
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="comments">
         {comments &&
           comments.map((comment) => (
-            <div key={comment.id}>
-              <div>{comment.username}</div>
-              <div>{comment.body}</div>
-              {comment.user_id == user.id && (
+            <div className="comment-container" key={comment.id}>
+              <div className="comment-user">
+                <div>{comment.profile_pic}</div>
+                <div style={{fontSize:"13px", marginTop:"10px", color:"#456e96", fontWeight:"bold"}}>{comment.username}</div>
+              </div>
+              <div className="comment-body">
                 <div>
+                  {comment.body}</div>
+                  <div style={{display:"flex", justifyContent:"end", alignItems:"end", marginTop:"50px", fontStyle:"italic", color:"gray"}}>
+                    {getTimeAgo(comment.created_at)}
+                  </div>
+              {comment.user_id == user.id && (
+                <div className="edit-delete-button-container">
                   <OpenModalButton
                     buttonText={"Edit"}
-                    modalComponent={<CommentModal comment={comment}/>}
+                    modalComponent={<CommentModal comment={comment} />}
                   />
-                  <button onClick={(()=> handleDeleteCommentClick(comment.id))}>Delete</button>
+                  <button className="delete-comment-button" onClick={() => handleDeleteCommentClick(comment.id)}>
+                    Delete
+                  </button>
                 </div>
               )}
+                  </div>
             </div>
           ))}
-      </ul>
+      </div>
     </div>
   );
 }
